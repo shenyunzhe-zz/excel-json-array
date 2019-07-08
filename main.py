879bc665fa46db6dfea9e91fcb760ced705f55eb
shenyz <shenyunzhe@outlook.com>
2019-07-08 18:54:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ cards = {
 excel_data = pd.read_excel('数据.xlsx')
 
 #将数据转成json json.loads(excel_data.to_json(force_ascii=False,orient='index'))
-#讲数据插入数组
+#将数据插入数组
 for (k,v) in json.loads(excel_data.to_json(force_ascii=False,orient='index')).items():
     cards["cards"].append(v)
 
